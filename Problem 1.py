29d7ff5a7e4298bf510abdc8a6b30fca44f8ff81
Ethan Breckenridge <ekbreckenridge@me.com>
2018-05-14 10:00:42 -0700

    Added Problem 1

---
+++ Problem 1.py
@@ -0,0 +1,19 @@
+## X is for all the multiples of 3, y is for all the multiples of 5, check's job is to remove the multiples of 5
+## from the multiples of 3, and sum is the sums of x and y 
+x = int(0)
+y = int(0)
+check = int(0)
+sum = int(0)
+unused = int(0)
+
+while x!= 999:
+	x+=3
+	check = x % 5
+	print(check)
+	if check == 0
+		unsued + x
+	else sum += x
+
+while y != 995
+	y +=5
+	
