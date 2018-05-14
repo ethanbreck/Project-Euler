1541c52f444f7448ee8461f6a143019cae70f241
Ethan Breckenridge <ekbreckenridge@me.com>
2018-05-14 15:18:45 -0700

    Fixed project 001 to become simpler

--- 001.py
+++ 001.py
@@ -1,21 +1,8 @@
-'''X is for all the multiples of 3, y is for all the multiples of 5, check's
-job is to remove the multiples of 5, from the multiples of 3, and sum is the
-sums of x and y for Project Euler Problem 1'''
+'''For project Euler project 1'''
 
-three = int(0)
-five = int(0)
-check = int(0)
-total = int(0)
-
-
-while three!= 999:
-	three+=3
-	check = three % 5
-	if check != 0:
-		total += three
-
-while five!=995:
-	five+=5
-	total += five
-
-print(total)
+total = 0
+for num in range(1000):
+	if num % 3 == 0 or num % 5 == 0:
+		print(num)
+		total = total + num
+print total
