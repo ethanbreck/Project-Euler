b34d9929c5ce2d86052b21800f6bcea8ee556d01
Ethan Breckenridge <ekbreckenridge@me.com>
2018-05-15 16:13:32 -0700

    Have basic code, issue with output staying at 1.

--- 002.py
+++ 002.py
@@ -1,7 +1,9 @@
 
-def fib(max):
-    x = 0
-    y = 1
-    while x < 4000000:
-        yield x
-        x, y = y, x + y
+def fib(n):
+    a, b = 1, 1
+    for i in range(n-1):
+        a, b = b, a+b
+        return a
+
+
+print(fib(400))
