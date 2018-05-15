a84ac825d1e833e08aa20cfaceae6faf7a838678
Ethan Breckenridge <ekbreckenridge@me.com>
2018-05-15 11:36:35 -0700

    Trying to figure out how printing function.

--- 002.py
+++ 002.py
@@ -2,11 +2,6 @@
 def fib(max):
     x = 0
     y = 1
-    while max > x:
+    while x < 4000000:
         yield x
         x, y = y, x + y
-
-
-for max in range(4000000):
-    sum(fib)
-    print(sum)
