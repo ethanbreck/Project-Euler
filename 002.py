0d6825eeebf6b2a45c93a0397b6039fdd9b182cb
Ethan Breckenridge <ekbreckenridge@me.com>
2018-05-16 09:34:55 -0700

    Working on Fibonnaci function

--- 002.py
+++ 002.py
@@ -1,9 +1,7 @@
-
 def fib(n):
-    a, b = 1, 1
+    a, b = 0, 1
     for i in range(n-1):
-        a, b = b, a+b
+        a = b
+        b = a+b
         return a
-
-
-print(fib(400))
+        print(fib(400))
