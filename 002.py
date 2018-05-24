16af81f375f4a2b4f6cb3859d1c9c1370b9d21a2
Ethan Breckenridge <ekbreckenridge@me.com>
2018-05-24 15:17:16 -0700

    Figured it out. Completed Problem 002

--- 002.py
+++ 002.py
@@ -16,8 +16,10 @@ def fibsum(n):
         old_a = a
         a, b = b, a + b
         print(old_a)
-    return old_a
-    if fibsum(4000000) % 2 == 0:
-        total += old_a
+        if old_a % 2 == 0:
+            total += old_a
         print(total)
-    return total
+    return old_a
+
+
+fibsum(4000000)
